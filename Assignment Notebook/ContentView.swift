8b5e081ca9cc1e71f1a823360178ecd5be4064da
Mia <miaelanderson@gmail.com>
2025-01-27 09:30:09 -0600

    Initial Commit

---
+++ Assignment Notebook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment Notebook
+//
+//  Created by Mia Anderson on 1/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
