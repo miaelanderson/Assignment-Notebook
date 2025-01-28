750f3d01f2674ff1888fac4c8d75bfe0fbf802a2
Mia <miaelanderson@gmail.com>
2025-01-28 10:04:39 -0600

    Just added list and edit button

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,17 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems = ["First Assignment", "Second Assignment", "Third Assignment", "Fourth Assignment", "Fifth Assignment"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(assignmentItems, id: \.self) { Assignment in
+                    Text(Assignment)
+                }
+                .onMove(perform: { indices, newOffset in
+                    assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    assignmentItems.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("Assignment Notebook", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+struct AssignmentItem: Identifiable, Codable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
+}
+
